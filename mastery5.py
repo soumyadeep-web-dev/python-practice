74408644f6bfc927af744a28779f3b901d59cdfd
soumyadeep-web-dev <soumyadeepchakraborty8777@hmail.com>
2026-06-04 18:15:06 +0530

    Die rolling programm

--- mastery5.py
+++ mastery5.py
@@ -29,3 +29,28 @@ dice_art={ 1:("┌─────────┐",
               "│         │ ",
               "│ ●     ● │ ",
               "└─────────┘"),
+           5:("┌─────────┐",
+              "│ ●     ● │ ",
+              "│    ●    │ ",
+              "│ ●     ● │ ",
+              "└─────────┘"),
+           6:("┌─────────┐",
+              "│ ●     ● │ ",
+              "│ ●     ● │ ",
+              "│ ●     ● │ ",
+              "└─────────┘"), }
+
+dice=[]
+total=0
+no_of_dice=int(input("Enter the number of times the die to be rolled:  "))
+
+for die in range(no_of_dice):
+   dice.append(random.randint(1,6))
+
+for num in dice:
+   for lines in dice_art.get(num):
+      print(lines)  
+
+for count in dice:
+   total+=count
+print(total)
